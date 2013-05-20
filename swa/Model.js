03fa5635844180b8c1c0b95f08bb8cfa8f867820
Lyle Troxell <lyle@troxell.com>
2013-05-19 18:15:25 -0700

    Initial commit

---
+++ swa/Model.js
@@ -0,0 +1,24 @@
+﻿
+guidedModel =// @startlock
+{
+	Person :
+	{
+		email :
+		{
+			events :
+			{
+				onLoad:function(attributeName)
+				{// @endlock
+					this.email = "*****";
+				}// @startlock
+			}
+		},
+		fullName :
+		{
+			onGet:function()
+			{// @endlock
+				return this.firstName + " " + this.lastName;
+			}// @startlock
+		}
+	}
+};// @endlock
---
+++ swa/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,33 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var createNewBlankPerson = {};	// @button
+	var savePerson = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	createNewBlankPerson.click = function createNewBlankPerson_click (event)// @startlock
+	{// @endlock
+		$('#personForm').show();
+	};// @lock
+
+	savePerson.click = function savePerson_click (event)// @startlock
+	{// @endlock
+		waf.sources.person.save({
+			onSuccess: function(event){
+				alert('Congradulations ' + waf.sources.person.fullName + ' you have been entered!');
+				$('#personForm').hide();
+			},
+			onError: function(event){
+				alert('Yikes, something went wrong...' + event.error['0'].message);				
+			}
+		})
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("createNewBlankPerson", "click", createNewBlankPerson.click, "WAF");
+	WAF.addListener("savePerson", "click", savePerson.click, "WAF");
+// @endregion
+};// @endlock
